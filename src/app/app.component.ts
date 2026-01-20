cb7e1986ae24ecc72220e2f77d6a67f824ad848c
Cristian <cleobla0802@iescarrillo.es>
2026-01-20 13:15:22 +0100

    Seguir la maquetacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { AfterViewInit, Component } from '@angular/core';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet ,NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
