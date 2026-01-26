891df4f7abcc06f72d19d1e65cd53bc22d080e9c
Cristian <cleobla0802@iescarrillo.es>
2026-01-26 14:06:24 +0100

    Seguir con lo del navbar y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { AfterViewInit, Component } from '@angular/core';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
 import { LoginComponent } from './componentes/auth/login/login.component';
 import { SiginComponent } from './componentes/auth/sigin/sigin.component';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,24 @@ import { SiginComponent } from './componentes/auth/sigin/sigin.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+showNavbar = true;
+  showFooter = true;
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute.root;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route.snapshot.data;
+      })
+    ).subscribe(data => {
+      this.showNavbar = data['showNavbar'] ?? true;
+      this.showFooter = data['showFooter'] ?? true;
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { LoginComponent } from './componentes/auth/login/login.component';
 import { SiginComponent } from './componentes/auth/sigin/sigin.component';
 
 export const routes: Routes = [
-    {path: 'componentes/presentacion' ,component:PresentacionComponent},
-    {path: 'componentes/auth/login' ,component:LoginComponent},
-    {path: 'componentes/auth/sigin' ,component:SiginComponent},
-    {path: 'componentes/home' ,component:HomeComponent},
+    {path: 'componentes/presentacion' ,component:PresentacionComponent, data: { showNavbar: false, showFooter: true } },
+    {path: 'componentes/auth/login' ,component:LoginComponent, data: { showNavbar: false, showFooter: true }},
+    {path: 'componentes/auth/sigin' ,component:SiginComponent, data: { showNavbar: false, showFooter: true }},
+    {path: 'componentes/home' ,component:HomeComponent, data: { showNavbar: true, showFooter: true }},
     {path: '',redirectTo:'componentes/presentacion',pathMatch:'full'},
     {path: '**',redirectTo:'componentes/presentacion',pathMatch:'full'}
 ];
