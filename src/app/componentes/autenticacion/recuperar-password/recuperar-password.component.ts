2ead7a87c2b3c19a2c777bfaade35ca5465d733a
Cleobla0802 <cleobla0802@iescarrillo.es>
2026-03-01 19:55:39 +0100

    Commit

--- src/app/componentes/autenticacion/recuperar-password/recuperar-password.component.ts
+++ src/app/componentes/autenticacion/recuperar-password/recuperar-password.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-password',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './recuperar-password.component.html',
   styleUrl: './recuperar-password.component.scss'
 })
