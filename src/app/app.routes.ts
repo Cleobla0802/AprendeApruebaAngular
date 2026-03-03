a4c9ccdfdf980265037d27c5bbdde3fa812658a6
Cleobla0802 <cleobla0802@iescarrillo.es>
2026-03-03 15:58:52 +0100

    Implementacion feature crear nueva contraseña finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { VisualizarApuntesComponent } from './componentes/apuntes/visualizar-apu
 import { CrearApuntesComponent } from './componentes/apuntes/crear-apuntes/crear-apuntes.component';
 import { RecuperarPasswordComponent } from './componentes/autenticacion/recuperar-password/recuperar-password.component';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { CrearNuevaPasswordComponent } from './componentes/autenticacion/recuperar-password/crear-nueva-password/crear-nueva-password.component';
 
 export const routes: Routes = [
     {path: 'componentes/presentacion' ,component:PresentacionComponent},
     {path: 'componentes/autenticacion/login' ,component:LoginComponent},
     {path: 'componentes/autenticacion/sigin' ,component:SiginComponent},
     {path: 'componentes/autenticacion/recuperar-password' ,component:RecuperarPasswordComponent},
+    {path: 'componentes/autenticacion/recuperar-password/crear-nueva-password' ,component:CrearNuevaPasswordComponent},
     {path: 'componentes/apuntes' ,component:ApuntesComponent, ...canActivate(()=>redirectUnauthorizedTo('/login'))},
     {path: 'componentes/apuntes/visualizar-apuntes', component: VisualizarApuntesComponent},
     {path: 'componentes/apuntes/crear-apuntes', component: CrearApuntesComponent },
